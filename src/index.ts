81f339650ce7de7fa193fabe4ecc63a0dcc551d1
Aaron Kromer <kromer.aaron@gmail.com>
2026-05-03 20:09:43 +0200

    removed unused shaders

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import * as THREE from 'three';
-import { sizes, SCENE_UTIL} from './utils';
+import { sizes, SCENE_UTIL } from './utils';
 import { camera, updatePlayer } from './player';
-import { sunWorldPos } from './sun';
-import { initComposer, renderGodRays } from './god-rays';
 import { labelRenderer } from './labels';
 import './world.ts';
 
@@ -14,18 +12,15 @@ light.position.set(0.5, 1, 0.75);
 SCENE_UTIL.scene.add(light);
 
 const canvas = document.querySelector('canvas.webgl');
-if (!canvas) throw new Error("Canvas element not found");
+if (!canvas) throw new Error('Canvas element not found');
 
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFShadowMap;
 renderer.setSize(sizes.width, sizes.height);
 
-const { composer, godRaysPass } = initComposer(renderer, SCENE_UTIL.scene, camera);
-
 renderer.setAnimationLoop(() => {
   updatePlayer();
-  //renderGodRays(renderer, sunWorldPos, camera, godRaysPass);
-  composer.render();
+  renderer.render(SCENE_UTIL.scene, camera);
   labelRenderer.render(SCENE_UTIL.scene, camera);
 });
