fba0242698c0bfb709e460110ed2639cceab9b7f
Aaron Kromer <kromer.aaron@gmail.com>
2026-05-07 14:38:42 +0200

    fix model loading url

--- src/graves.ts
+++ src/graves.ts
@@ -33,7 +33,7 @@ const pickPosition = (placed: Array<{ x: number; z: number }>): { x: number; z: 
 };
 
 export const buildGraves = async (): Promise<void> => {
-  const gltf = await SCENE_UTIL.loader.loadAsync('/src/assets/models/grave_1.gltf');
+  const gltf = await SCENE_UTIL.loader.loadAsync(new URL('./assets/models/grave_1.gltf', import.meta.url).href);
 
   gltf.scene.traverse((node) => {
     if ((node as THREE.Mesh).isMesh) {
